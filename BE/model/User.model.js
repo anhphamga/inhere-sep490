45aa96c15fbe4a3f7206f3303510564144d0eb20
tahp410 <ddphat410@gmail.com>
2026-02-22 11:50:04 +0700

    homepage role staff

--- BE/model/User.model.js
+++ BE/model/User.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   role: {
     type: String,
-    enum: ['owner', 'customer'],
+    enum: ['owner', 'staff', 'customer'],
     required: true,
     default: 'customer'
   },
